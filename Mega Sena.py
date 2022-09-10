5b4f21fc8d0fb52eb45fa792d6ba30154388db10
Chemdexx <97565038+Chemdexx@users.noreply.github.com>
2022-09-09 23:00:06 -0300

    Add files via upload
    This program generates the desired amount of random numbers from a predetermined interval.

---
+++ Mega Sena.py
@@ -0,0 +1,11 @@
+import random
+
+for i in range(6):
+    value = random.randint(1, 60)
+    print(value, end = " ")
+    
+print( )   
+
+mega = []
+for i in range (6):
+    print(random.choice(mega), end = " ")
